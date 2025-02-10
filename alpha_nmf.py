7642fd2c210aad8c0deee982e655c8ad05538de1
Yasaman Torabi <158237097+Torabiy@users.noreply.github.com>
2025-02-10 01:32:30 -0500

    Create alpha_nmf.py

---
+++ alpha_nmf.py
@@ -0,0 +1,24 @@
+# alpha_nmf.py
+# This module performs Alpha-NMF with regularization on the H matrix.
+
+import numpy as np
+from sklearn.decomposition import NMF
+import mir_eval
+
+def perform_alpha_nmf(Y, options, X, fs):
+    """Performs Alpha Non-Negative Matrix Factorization (NMF) on the mixed signals."""
+    model = NMF(n_components=options['J'], init=options['init'], max_iter=options['niter'],
+                alpha_H=options['alpha_H'])
+    W = model.fit_transform(Y.T)
+    H = model.components_
+    
+    # Reconstruct the signals from the components
+    reconstructed_signals = np.dot(W, H)
+    
+    # Evaluate separation performance
+    sdr, sir, sar, _ = mir_eval.separation.bss_eval_sources(X, reconstructed_signals)
+    print("Alpha-NMF SIR:", sir)
+    print("Alpha-NMF SDR:", sdr)
+    print("Alpha-NMF SAR:", sar)
+    
+    return H, reconstructed_signals
